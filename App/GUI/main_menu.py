6de8522b96c9869d3c859c550a3a883295356c0c
VillainousSsnake <cjdiamond9@gmail.com>
2024-02-25 11:52:50 -0500

    Added codee to main_menu function

--- App/GUI/main_menu.py
+++ App/GUI/main_menu.py
@@ -6,4 +6,10 @@ import customtkinter as ctk
 
 # main_menu function
 def main_menu(self):
-    pass  # TODO: Stub
+
+    # Creating root window
+    root = ctk.CTk()
+    root.title("AINB-Toolbox - VillainousSsnake - Alpha V0.1")
+
+    # Root mainloop
+    root.mainloop()
