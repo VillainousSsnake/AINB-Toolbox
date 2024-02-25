e2429a18e429eaa2068ea9b363a920fcbc7170ad
VillainousSsnake <cjdiamond9@gmail.com>
2024-02-25 15:10:03 -0500

    Added settings to settings menu, coded themes

--- App/GUI/main_menu.py
+++ App/GUI/main_menu.py
@@ -5,10 +5,21 @@ from tkinterdnd2 import DND_FILES
 import App.GUI.customtkinter as ctk
 from tkinter import filedialog
 from functools import partial
+from App.AppLib.config import Config
 
 
 # _func class
 class _Func:
+    @staticmethod
+    def update_theme_option_menu(theme_option_menu):
+        match Config.get_setting("current_theme"):
+            case "dark":
+                theme_option_menu.set("Dark")
+            case "light":
+                theme_option_menu.set("Light")
+            case "system":
+                theme_option_menu.set("System")
+
     @staticmethod
     def drop_file(tabview, open_file, event=None):
         open_file = event.data[1:len(event.data)-1]
@@ -17,6 +28,12 @@ class _Func:
 
 # Button func
 class ButtonFunc:
+    @staticmethod
+    def theme_option_menu_button_command(app, appearance):
+        ctk.set_appearance_mode(appearance.lower())
+        app.settings["current_theme"] = appearance.lower()
+        Config.overwrite_setting("current_theme", appearance.lower())
+
     @staticmethod
     def drag_and_drop_button_command(tabview, open_file, event=None):
 
@@ -41,6 +58,9 @@ class ButtonFunc:
 
 # main_menu function
 def main_menu(app):
+
+    # Setting theme
+    ctk.set_appearance_mode(app.settings["current_theme"])
 
     # Creating variables
     open_file = None
@@ -98,12 +118,29 @@ def main_menu(app):
     romfs_path_label = ctk.CTkLabel(
         master=tabview.tab("Settings"),
         text="Game Dump Location",
+        corner_radius=5, fg_color="#3B8ED0"
     )
     romfs_path_label.place(x=50)
 
     romfs_path_entry = ctk.CTkEntry(master=tabview.tab("Settings"),
                                     placeholder_text="Eg. (D:\\Tears of the Kingdom\\romfs)")
-    romfs_path_entry.place(x=200)
+    romfs_path_entry.pack()
+
+    theme_label = ctk.CTkLabel(
+        master=tabview.tab("Settings"),
+        text="Current Theme            ",
+        corner_radius=5, fg_color="#3B8ED0"
+    )
+    theme_label.place(x=50, y=50)
+
+    theme_option_menu_command = partial(ButtonFunc.theme_option_menu_button_command, app)
+    theme_option_menu = ctk.CTkOptionMenu(
+        master=tabview.tab("Settings"),
+        values=["Dark", "Light", "System"],
+        command=theme_option_menu_command
+    )
+    _Func.update_theme_option_menu(theme_option_menu)
+    theme_option_menu.pack(pady=20)
 
     # Root mainloop
     root.mainloop()
