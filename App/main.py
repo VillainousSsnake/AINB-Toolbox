d1e28f6931891d0475be2d7cf1c59933febc48f6
VillainousSsnake <cjdiamond9@gmail.com>
2024-02-25 11:55:22 -0500

    Added comments to main.py

--- App/main.py
+++ App/main.py
@@ -1,11 +1,14 @@
 # App/main.py
 # Holds main program code
 
+# Importing modules
 from App.AppLib.app import App
 from App.AppLib.index import Index
 
+# Creating App
 app = App()
 
+# Main loop
 while app.returnStatement != "exit":
 
     match app.returnStatement:
