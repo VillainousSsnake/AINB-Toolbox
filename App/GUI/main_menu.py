3e938a98756d296096bb56ef45da632c9b03eb08
VillainousSsnake <cjdiamond9@gmail.com>
2024-02-25 11:58:01 -0500

    Added code to main_menu func, refactored param in main_menu func

--- App/GUI/main_menu.py
+++ App/GUI/main_menu.py
@@ -5,11 +5,19 @@ import customtkinter as ctk
 
 
 # main_menu function
-def main_menu(self):
+def main_menu(app):
 
     # Creating root window
     root = ctk.CTk()
     root.title("AINB-Toolbox - VillainousSsnake - Alpha V0.1")
+
+    # Defining on_close function
+    def on_close():
+        root.destroy()
+        app.returnStatement = "exit"
+
+    # Assigning the buttons on the tkinter window top bar
+    root.protocol("WM_DELETE_WINDOW", on_close)
 
     # Root mainloop
     root.mainloop()
