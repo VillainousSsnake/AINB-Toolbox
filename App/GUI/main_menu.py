72a7ebfe042a633465131a5fa3718a953e557502
VillainousSsnake <cjdiamond9@gmail.com>
2024-02-25 14:01:14 -0500

    Added tabview, added command to dragAndDropTarget

--- App/GUI/main_menu.py
+++ App/GUI/main_menu.py
@@ -3,6 +3,7 @@
 # Importing dependencies
 from tkinterdnd2 import DND_FILES
 import App.GUI.customtkinter as ctk
+from tkinter import filedialog
 
 
 # _func class
@@ -15,12 +16,26 @@ class _Func:
 # Button func
 class ButtonFunc:
     @staticmethod
-    def app_section_header_button_command(value: str = None):
-        pass  # TODO: Stub
+    def drag_and_drop_button_command(event=None):
+
+        supportedFileFormats = (
+            (
+                'All Editable Files', [
+                    '*.ainb'
+                ]
+            ),
+            ('AI Node Binary', '*.ainb')
+        )
+
+        filepath = filedialog.askopenfile(title="Select a file", filetypes=supportedFileFormats)
 
 
 # main_menu function
 def main_menu(app):
+
+    # Creating variables
+    open_file = None
+    file_specs = None
 
     # Creating root window
     root = ctk.CTk()
@@ -35,18 +50,22 @@ def main_menu(app):
     # Assigning the buttons on the tkinter window top bar
     root.protocol("WM_DELETE_WINDOW", on_close)
 
-    # Creating app_sections_header button
-    app_sections_header = ctk.CTkSegmentedButton(root,
-                                                 values=["Home", "AINB Editor", "Settings"],
-                                                 command=ButtonFunc.app_section_header_button_command)
-    app_sections_header.set("Home")
-    app_sections_header.pack()
+    # Creating tabview
+    tabview = ctk.CTkTabview(root)
+    tabview.add("Home")
+    tabview.add("AINB Editor")
+    tabview.add("Settings")
+    tabview.set("Home")
+    tabview.pack(fill=ctk.BOTH)
 
-    dragAndDropTarget = ctk.CTkLabel(root, text="➕ \nDrag & Drop Here", corner_radius=10, fg_color="gray", wraplength=300)
-    dragAndDropTarget.pack(expand=True, fill="both", padx=40, pady=40)
+    dragAndDropTarget = ctk.CTkLabel(tabview.tab("Home"),
+                                     text="➕ \nDrag & Drop Here",
+                                     corner_radius=10, fg_color="gray", wraplength=300)
+    dragAndDropTarget.pack(expand=True, fill=ctk.BOTH, padx=40, pady=40)
 
     dragAndDropTarget.drop_target_register(DND_FILES)
     dragAndDropTarget.dnd_bind("<<Drop>>", _Func.drop_file)
+    dragAndDropTarget.bind("<1>", ButtonFunc.drag_and_drop_button_command)
 
     # Root mainloop
     root.mainloop()
