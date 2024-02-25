eb81a61e5d8f8136704bbc3828821ffb142d0536
VillainousSsnake <cjdiamond9@gmail.com>
2024-02-25 12:39:44 -0500

    Added _Func class and drop_file method to _Func

--- App/GUI/main_menu.py
+++ App/GUI/main_menu.py
@@ -2,7 +2,14 @@
 
 # Importing dependencies
 import customtkinter as ctk
+from tkinterdnd2 import DND_FILES
 
+
+# _func class
+class _Func:
+    @staticmethod
+    def drop_file(event: str = None):
+        pass  # TODO: Stub
 
 # Button func
 class ButtonFunc:
@@ -33,6 +40,13 @@ def main_menu(app):
                                                  command=ButtonFunc.app_section_header_button_command)
     app_sections_header.set("Home")
     app_sections_header.pack()
+
+    label = ctk.CTkLabel(root, text="➕ \nDrag & Drop Here", corner_radius=10, fg_color="blue", wraplength=300)
+    label.pack(expand=True, fill="both", padx=40, pady=40)
+
+    # Add this 2 lines to make it a dnd widget
+    label.drop_target_register(DND_FILES)
+    label.dnd_bind('<<Drop>>', drop)
 
     # Root mainloop
     root.mainloop()
