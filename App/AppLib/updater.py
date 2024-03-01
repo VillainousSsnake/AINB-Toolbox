e92492ebec66034ded8bf433327eefe67a217f15
VillainousSsnake <cjdiamond9@gmail.com>
2024-03-01 12:13:40 -0500

    Added is_latest_update function

--- App/AppLib/updater.py
+++ App/AppLib/updater.py
@@ -4,9 +4,20 @@
 from github import Github
 
 
+# Creating global vars
+UpdateTagInt = 3
+
+
 class Updater:
 
     @staticmethod
-    def check_for_updates():
+    def is_latest_update():
 
-        pass  # TODO: Stub
+        github_controller = Github(None)
+        repo = github_controller.get_repo("VillainousSsnake/AINB-Toolbox")
+        release_tags = repo.get_releases().totalCount
+
+        if UpdateTagInt < release_tags:
+            return False
+
+        return True
